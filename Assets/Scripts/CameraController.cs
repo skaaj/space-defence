d506fe49c72aca590dbcd53c7000303d5d722f59
Benjamin Denom <ben.denom@gmail.com>
2015-12-23 12:08:56 +0100

    Initial commit with a bunch of standard assets

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float paneSpeed = 1.0f;
+    public float scrollSpeed = 4.0f;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal") * paneSpeed;
+        float vertical = Input.GetAxis("Vertical") * paneSpeed;
+        float zoom = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+
+        // Handle Camera Movement
+        Vector3 movement = new Vector3(horizontal, vertical, 0.0f);
+        transform.Translate(movement);
+
+        // Handle Camera Zoom
+        Camera mainCamera = Camera.main;
+        mainCamera.orthographicSize += zoom;
+        if (mainCamera.orthographicSize < 5)
+            mainCamera.orthographicSize = 5;
+    }
+}
